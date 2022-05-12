281b01a4f4d08a7d62a4ea719331b2e8947ba142
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-05-12 11:44:58 -0500

    Make button animated

--- App/components/Button.tsx
+++ App/components/Button.tsx
@@ -1,5 +1,41 @@
 import React from "react";
 import { Pressable, Text, StyleSheet } from "react-native";
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+const AnimatedView = ({
+  pressed,
+  title,
+}: {
+  pressed: boolean;
+  title: string;
+}) => {
+  const transform = useSharedValue(1);
+
+  const btnStyle = [
+    styles.button,
+    { backgroundColor: pressed ? "#9ff9e1" : "#5cebdf" },
+  ];
+
+  React.useEffect(() => {
+    transform.value = withTiming(pressed ? 1.1 : 1);
+  }, [pressed]);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: transform.value }],
+    };
+  });
+
+  return (
+    <Animated.View style={[...btnStyle, animatedStyles]}>
+      <Text style={styles.text}>{title}</Text>
+    </Animated.View>
+  );
+};
 
 type Props = {
   title: string;
@@ -8,14 +44,8 @@ type Props = {
 
 export const Button = ({ title, onPress }: Props) => {
   return (
-    <Pressable
-      onPress={onPress}
-      style={({ pressed }) => [
-        styles.button,
-        { backgroundColor: pressed ? "#9ff9e1" : "#5cebdf" },
-      ]}
-    >
-      <Text style={styles.text}>{title}</Text>
+    <Pressable onPress={onPress}>
+      {({ pressed }) => <AnimatedView pressed={pressed} title={title} />}
     </Pressable>
   );
 };
