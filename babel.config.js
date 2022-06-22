41cd0d9496ab1863535f27a893bb17c74bfdcb73
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-06-22 10:53:05 -0500

    Init project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            src: "./src",
+            components: "./src/components",
+            constants: "./src/constants",
+            context: "./src/context",
+            hooks: "./src/hooks",
+            navigation: "./src/navigation",
+            screens: "./src/screens",
+          },
+        },
+      ],
+    ],
   };
 };
